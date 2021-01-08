5dc1057dc7c28607fc2a5642c21832fd02e48f48
Harshdeep Dhiman <dhiman.harshdeep18@gmail.com>
2021-01-08 11:30:59 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+// npm i express express-validator bcryptjs config gravatar jsonwebtoken mongoose request
+//
+
+const express=require('express')
+
+const app=express()
+
+app.get('/',(req,res)=>{
+      res.send('Running')
+})
+
+const PORT=process.env.PORT||5000
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on ${PORT}`)
+})
